74fa0944490d5c0be2c3f2a70249fe6ea6e6adf1
Morgan Galvin <morgan@ludwigthepig.com>
2019-06-22 10:53:57 -0600

    fixed disconnect emit error

--- controllers/socket.js
+++ controllers/socket.js
@@ -3,14 +3,15 @@ const connection = (server) => {
 
   
   io.on('connection', (socket) => {
-    
     console.log('A user connected!')
     connectPlayer(socket);
     console.log(players)
     
     socket.on('disconnect', () => {
-      console.log('user disconnected');
-      disconnectPlayer(socket);
+      console.log('user disconnected'); 
+      const { id } = socket;
+      delete players[id]
+      io.emit('disconnect', id);
     });
     
   });
@@ -22,7 +23,7 @@ const connectPlayer = (socket) => {
   const id = socket.id;
   
   const playerObj = {
-    rotation: 0,
+    rotation: 0, 
     x: Math.floor(Math.random() * 700) + 50,
     y: Math.floor(Math.random() * 500) + 50,
     playerId: id,
@@ -32,12 +33,6 @@ const connectPlayer = (socket) => {
   players[id] = playerObj;
   socket.emit('currentPlayers', players)
   socket.broadcast.emit('newPlayer', players[id])
-}
-
-const disconnectPlayer = (socket) => {
-  const { id } = socket;
-  delete players[id]
-  io.emit('disconnect', id);
 }
 
 
